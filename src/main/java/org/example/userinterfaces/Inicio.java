17f392ff74f967ebc79dd02506513bb8b8408df3
DevJuan001 <juanesyt7@gmail.com>
2026-06-04 15:46:49 -0500

    feat: add Inicio page object for the main user interface

---
+++ src/main/java/org/example/userinterfaces/Inicio.java
@@ -0,0 +1,9 @@
+package org.example.userinterfaces;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.thucydides.core.annotations.DefaultUrl;
+
+@DefaultUrl("https://practicesoftwaretesting.com/")
+
+public class Inicio extends PageObject {
+}
