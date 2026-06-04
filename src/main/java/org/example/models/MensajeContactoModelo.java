95afa53e9de6b57727c92518c30749ba37a728ac
DevJuan001 <juanesyt7@gmail.com>
2026-06-04 16:24:46 -0500

    feat: add MensajeContactoModelo class for contact message representation

---
+++ src/main/java/org/example/models/MensajeContactoModelo.java
@@ -0,0 +1,14 @@
+package org.example.models;
+
+public class MensajeContactoModelo {
+    private String asunto;
+    private String mensaje;
+
+    public String getAsunto() {
+        return asunto;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+}
